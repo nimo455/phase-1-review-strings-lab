1bb2b7ba0dac258c5a2d2d5e669231bb609eba98
nimo455 <faizamaalim456@gmail.com>
2026-01-12 19:38:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser="SHINGA";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+
+const shortGreeting = 'Welcome, ' + currentUser.charAt(0) + '!';
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
